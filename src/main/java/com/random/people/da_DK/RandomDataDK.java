a60beb8de6f7143c354aa9194f91eb56fe2088f7
RokiShopen <marinkovic.aleksandarr@gmail.com>
2016-04-02 21:30:24 +0200

    Define minimum set of supported locales #1  -Added new locales  -Edited README.md  -Added file format to namePrefix.txt

---
+++ src/main/java/com/random/people/da_DK/RandomDataDK.java
@@ -0,0 +1,76 @@
+package com.random.people.da_DK;
+
+import com.random.people.RandomData;
+
+import java.util.Currency;
+import java.util.Date;
+
+/**
+ * @author sasa - marinkovic.aleksandarr@gmail.com on 4/2/2016
+ */
+public class RandomDataDK implements RandomData{
+    @Override
+    public String namePrefix() {
+        return null;
+    }
+
+    @Override
+    public String firstName() {
+        return null;
+    }
+
+    @Override
+    public String lastName() {
+        return null;
+    }
+
+    @Override
+    public String ssn() {
+        return null;
+    }
+
+    @Override
+    public String gender() {
+        return null;
+    }
+
+    @Override
+    public Date dateOfBirth() {
+        return null;
+    }
+
+    @Override
+    public String address() {
+        return null;
+    }
+
+    @Override
+    public String city() {
+        return null;
+    }
+
+    @Override
+    public String state() {
+        return null;
+    }
+
+    @Override
+    public String country() {
+        return null;
+    }
+
+    @Override
+    public String phoneNumber() {
+        return null;
+    }
+
+    @Override
+    public String nationality() {
+        return null;
+    }
+
+    @Override
+    public Currency currency() {
+        return null;
+    }
+}
