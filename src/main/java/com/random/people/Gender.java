6f53bba1423bb06d1fae520698367ecfdb027913
Aleksandar Marinkovic <marinkovic.aleksandarr@gmail.com>
2017-06-08 08:44:43 +0200

    Create logic for matching name and sex #14 - Refactoring

--- src/main/java/com/random/people/Gender.java
+++ src/main/java/com/random/people/Gender.java
@@ -1,18 +1,27 @@
-//==============================================================================
-// Copyright (c) 2017 by NOVOMATIC Lottery Solutions GmbH.
-// Wiedner Hauptstrasse 94, 1050 Vienna, Austria
-// All rights reserved.
-//
-// This software is the confidential and proprietary information
-// of NOVOMATIC Lottery Solutions GmbH ("Confidential Information").You
-// shall not disclose such Confidential Information and shall use
-// it only in accordance with the terms of the license agreement
-// you entered into with NOVOMATIC Lottery Solutions GmbH.
-//==============================================================================
+/**
+ * This file is part of Random data generator.
+ * <p>
+ * Random data generator is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * <p>
+ * Random data generator is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * <p>
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Random data generator.
+ * If not, see <http://www.gnu.org/licenses/>.
+ */
 package com.random.people;
 
 /**
- * @author sasa - sasa@novomaticls.com
+ * Random data enum that is used for different genders.
+ * @author Aleksandar Marinkovic (marinkovic.aleksandarr@gmail.com)
+ * @version $Id$
+ * @since 0.0.1
  */
 public enum Gender
 {
